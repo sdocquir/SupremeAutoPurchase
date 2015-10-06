82c9cdfa03b20195aaba995190926492570591b5
EdwardHuCS <edward.s.hu.cs@gmail.com>
2015-10-06 13:38:38 -0700

    Updated for Python 3

--- buy.py
+++ buy.py
@@ -15,68 +15,73 @@ phonefield = "5555555555"
 addressfield = "1600 Pennsylvania Avenue NW"
 zipfield = "20500"
 statefield = "DC"
-cctypefield = "master" # "master" "visa" "american_express"
-ccnumfield = "5274576954806318" # Randomly Generated Data (aka, this isn't mine)
-ccmonthfield = "06" # Randomly Generated Data (aka, this isn't mine)
-ccyearfield = "2019" # Randomly Generated Data (aka, this isn't mine)
-cccvcfield = "800" # Randomly Generated Data (aka, this isn't mine)
+cctypefield = "master"  # "master" "visa" "american_express"
+ccnumfield = "5274576954806318"  # Randomly Generated Data (aka, this isn't mine)
+ccmonthfield = "06"  # Randomly Generated Data (aka, this isn't mine)
+ccyearfield = "2019"  # Randomly Generated Data (aka, this isn't mine)
+cccvcfield = "800"  # Randomly Generated Data (aka, this isn't mine)
+
 
 def main():
-	r = requests.get(mainUrl).text
-	if product in r: 
-		parse(r)
+    r = requests.get(mainUrl).text
+    if product in r:
+        parse(r)
+
 
 def parse(r):
-        soup = BeautifulSoup(r, "html.parser")
-	for a in soup.find_all('a', href=True):
-    		link = a['href']
-		checkproduct(link)
+    soup = BeautifulSoup(r, "html.parser")
+    for a in soup.find_all('a', href=True):
+        link = a['href']
+        checkproduct(link)
+
 
 def checkproduct(l):
-	if product in l:
-		prdurl = baseUrl + l
-		print prdurl
-		buyprd(prdurl)
+    if product in l:
+        prdurl = baseUrl + l
+        print(prdurl)
+        buyprd(prdurl)
+
 
 def buyprd(u):
-	browser = Browser('firefox')
-	url = u
-	browser.visit(url)
-	# 10|10.5
-	browser.find_option_by_text(selectOption).first.click()
-	browser.find_by_name('commit').click()
-	if browser.is_text_present('item'):
-		print("Added to Cart")
-	else:
-        	print("Error")
-		return
-	print "checking out"
-	browser.visit(checkoutUrl)
-	print "Filling Out Billing Info"
-	browser.fill("order[billing_name]", namefield)
-	browser.fill("order[email]", emailfield)
-	browser.fill("order[tel]", phonefield)
+    browser = Browser('firefox')
+    url = u
+    browser.visit(url)
+    # 10|10.5
+    browser.find_option_by_text(selectOption).first.click()
+    browser.find_by_name('commit').click()
+    if browser.is_text_present('item'):
+        print("Added to Cart")
+    else:
+        print("Error")
+        return
+    print("checking out")
+    browser.visit(checkoutUrl)
+    print("Filling Out Billing Info")
+    browser.fill("order[billing_name]", namefield)
+    browser.fill("order[email]", emailfield)
+    browser.fill("order[tel]", phonefield)
 
-	print "Filling Out Address"
-	browser.fill("order[billing_address]", addressfield)
-	browser.fill("order[billing_zip]", zipfield)
-	browser.select("order[billing_state]", statefield)
-	print "Filling Out Credit Card Info"
+    print("Filling Out Address")
+    browser.fill("order[billing_address]", addressfield)
+    browser.fill("order[billing_zip]", zipfield)
+    browser.select("order[billing_state]", statefield)
+    print("Filling Out Credit Card Info")
 
-	browser.select("credit_card[type]", cctypefield)
-	browser.fill("credit_card[number]", ccnumfield)
-	browser.select("credit_card[month]", ccmonthfield)
-	browser.select("credit_card[year]", ccyearfield)
-	browser.fill("credit_card[verification_value]", cccvcfield)
-	browser.find_by_css('.terms').click()
-	print "Submitting Info"
-	browser.find_by_name('commit').click()
-	sys.exit(0)
+    browser.select("credit_card[type]", cctypefield)
+    browser.fill("credit_card[number]", ccnumfield)
+    browser.select("credit_card[month]", ccmonthfield)
+    browser.select("credit_card[year]", ccyearfield)
+    browser.fill("credit_card[verification_value]", cccvcfield)
+    browser.find_by_css('.terms').click()
+    print("Submitting Info")
+    browser.find_by_name('commit').click()
+    sys.exit(0)
+
 
 i = 0
 
-while(True):
-	main()
-	print "On try number " + str(i)
-	i = i + 1
-	time.sleep(2)
+while (True):
+    main()
+    print("On try number " + str(i))
+    i = i + 1
+    time.sleep(2)
