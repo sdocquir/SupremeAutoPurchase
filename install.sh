8d717afc0d81229c7c3eaa068235310f35cd09bd
Whit Jack <whitj00@ymail.com>
2015-10-05 22:07:05 -0700

    first commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python2.7
+sudo pip install -r requirements.txt
+ECHO " __________ 
+< Let's go!>
+ ---------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+
+                ||----w |
+                ||     ||"
+say "Lets go " `whoami`
